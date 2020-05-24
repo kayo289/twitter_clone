cfdfdd8b9ae22c7c82775e1e5b4bd0f599779380
kayo289 <kayokapi@icloud.com>
2020-05-25 03:44:09 +0900

    [make]post good follow followerのmodel

--- snsapp/models.py
+++ snsapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class PostModel(models.Model):
+    user_id = models.IntegerField()
+    content = models.TextField(max_length = 140)
+    images = models.ImageField(upload_to="post")
+
+class GoodModel(models.Model):
+    user_id = models.IntegerField()
+    post_id = models.IntegerField()
+    count_id = models.IntegerField()
+
+class FollowModel(models.Model):
+    user_id = models.IntegerField()
+    follow_id = models.IntegerField()
+
+class FollowerwModel(models.Model):
+    user_id = models.IntegerField()
+    follower_id = models.IntegerField()
