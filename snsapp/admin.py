bd4c60b85471d48abc39b5cd313df1dd5362188c
kayo289 <kayokapi@icloud.com>
2020-05-26 12:26:52 +0900

    finish

--- snsapp/admin.py
+++ snsapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import PostModel,GoodModel,FollowModel,IntoroductionModel
 
 # Register your models here.
+admin.site.register(PostModel)
+admin.site.register(GoodModel)
+admin.site.register(FollowModel)
+admin.site.register(IntoroductionModel)
