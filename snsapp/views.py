fc5b4c9bec5f09d12afaf48a3c69a9e2a13879e3
kayo289 <kayokapi@icloud.com>
2020-05-29 03:18:39 +0900

    finish ログイン・ログアウト機能

--- snsapp/views.py
+++ snsapp/views.py
@@ -1,5 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 def aaa():
     return 'a'
+
+def signupfunc(request):
+    # user2 = User.objects.all()
+    if request.method == 'POST':
+        user_name = request.POST['username']
+        user_email = request.POST['email']
+        user_password = request.POST['password']
+
+        if User.objects.filter(username = user_name, email=user_email, password=user_password).exists():
+            # 複数条件を利用したいからfilterを利用
+            User.objects.filter(email=user_email, password=user_password)
+            return render(request, 'signup.html', {'error': 'このユーザーは登録されています'})
+
+        User.objects.create_user(user_name, user_email, user_password)
+        user = authenticate(request, username=user_name, password=user_password)
+        login(request, user)
+        return render(request, 'signup.html', {'error': 'ログイン成功'}) #ログインが成功した時の画面移動先
+    return render(request, 'signup.html', {'error': 'こないはず'}) #これがないとエラーが出る。Djangoの仕様かもしれない
+
+def loginfunc(request):
+    if request.method == 'POST':
+        user_name = request.POST['username']
+        user_password = request.POST['password']
+        # 権限確認らしい
+        user = authenticate(request, username=user_name, password=user_password)
+        if user is not None:
+            login(request, user)
+            return redirect('signup')
+        else:
+            return redirect('login')# ログイン成功後の遷移先
+    return render(request,'login.html')
