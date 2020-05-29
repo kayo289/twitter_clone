25a12992a8ec6be733baccce9617a6819b6603d4
kayo289 <kayokapi@icloud.com>
2020-05-29 21:28:35 +0900

    buf

--- snsapp/models.py
+++ snsapp/models.py
@@ -1,22 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User
 
 # Create your models here.
 class PostModel(models.Model):
-    user_id = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField(max_length = 140)
     images = models.ImageField(upload_to="post")
 
 class GoodModel(models.Model):
-    user_id = models.IntegerField()
-    post_id = models.IntegerField()
-    count_id = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(PostModel, on_delete=models.CASCADE)
+    count = models.IntegerField()
 
 class FollowModel(models.Model):
-    user_id = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     follow_id = models.IntegerField()
 
 class IntoroductionModel(models.Model):
-    user_id = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     introduction = models.TextField(max_length = 140)
     url = models.URLField(max_length=200)
