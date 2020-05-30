d31431e45eb770d0b6f9d1344f024467dee44025
abema40 <saotsuru.sho@gmail.com>
2020-05-30 17:52:15 +0900

    pullのため

--- snsapp/urls.py
+++ snsapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from .views import aaa
+from .views import aaa,Mypage
 
 urlpatterns = [
     path('a/', aaa)
+
 ]
--- snsapp/views.py
+++ snsapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import PostModel,FollowModel,GoodModel,IntoroductionModel
 
 # Create your views here.
 def aaa():
     return 'a'
+
