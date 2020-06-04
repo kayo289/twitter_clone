f8f41d0cfe0a0d4e0e704b632f7db08e34c569de
kayo289 <kayokapi@icloud.com>
2020-06-05 03:20:25 +0900

    finish ログアウト機能

--- snsapp/urls.py
+++ snsapp/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import mypage, signupfunc, loginfunc, index_post,followfunc,follow_delete,goodfunc,create_posts
+from .views import mypage, signupfunc, loginfunc, index_post,followfunc,follow_delete,goodfunc,create_posts,logoutfunc
 
 #signupfunc, loginfunc, index_post ,
 urlpatterns = [
     path('signup/', signupfunc, name='signup'),
     path('login/',loginfunc, name='login'),
+    path('logout/',logoutfunc, name='logout'),
     path('list/',index_post, name='index_post'),
     path('mypage/<int:pk>', mypage,name="mypage"),
     path('follow/<int:pk>',followfunc,name='follow'),
